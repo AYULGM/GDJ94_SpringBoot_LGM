43ecdb8bfcc6b01cddd6c7d81476224d5c77c5e4
AYULGM <dlrudals393@naver.com>
2025-11-24 17:34:15 +0900

    init~~~

---
+++ src/main/java/com/winter/app/Gdj94SpringBootLgmApplication.java
@@ -0,0 +1,13 @@
+package com.winter.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gdj94SpringBootLgmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gdj94SpringBootLgmApplication.class, args);
+	}
+
+}
